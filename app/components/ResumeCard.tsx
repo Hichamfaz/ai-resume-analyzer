63c8bb6f4be83eb55799f6d94fda4fa7f0809028
Hicham El Fazazi <hichamelfazazi.hef@gmail.com>
2025-08-15 12:37:41 +0200

    Implement wipe functionality

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -35,7 +35,7 @@ const ResumeCard = ({ resume:{id,companyName,jobTitle,imagePath,resumePath,feedb
         {resumeUrl && (      <div className="gradient-border animate-in fade-in duration-1000">
        <div className="w-full h-full">
           <img 
-          src={imagePath} 
+          src={resumeUrl} 
           alt="resume" 
           className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
           />
